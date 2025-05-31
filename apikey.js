0c9e8095e1f21b05f216f0959a3a163ad61bb752
사과포럼 <118648952+sagwaforum@users.noreply.github.com>
2025-05-31 21:26:01 +0900

    .

---
+++ apikey.js
@@ -0,0 +1,12 @@
+ const firebaseConfig = {
+  FB_API_KEY1: "AIzaSyCMwlM_ihNsq6UaM7VSyqS-rxEJTAEBX-E",
+  FB_API_KEY2: "sagwaforum.firebaseapp.com",
+  FB_API_KEY3: "https://sagwaforum-default-rtdb.asia-southeast1.firebasedatabase.app",
+  FB_API_KEY4: "sagwaforum",
+  FB_API_KEY5: "sagwaforum.firebasestorage.app",
+  FB_API_KEY6: "172362815284",
+  FB_API_KEY7: "1:172362815284:web:294bcbd26174464911f761",
+  FB_API_KEY8: "G-YBRP27F0VP"
+};
+
+
--- firebase.js
+++ firebase.js
@@ -1,32 +1,16 @@
+import firebaseConfig from "./apikey.js"
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.8.1/firebase-app.js";
+import { getDatabase } from "firebase/database";
 
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/11.8.1/firebase-app.js";
-  import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.8.1/firebase-analytics.js";
-  import { getDatabase } from "firebase/database";
-  import { getDocs } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-firestore.js";
-
-
-// TODO: Replace the following with your app's Firebase project configuration
-// See: https://firebase.google.com/docs/web/learn-more#config-object
 const firebaseConfig = {
-  const firebaseConfig = {
-    
-  };
-
-  // ...
-  // The value of `databaseURL` depends on the location of the database
-  databaseURL: "https://DATABASE_NAME.firebaseio.com",
+  apiKey: firebaseConfig.FB_API_KEY1,
+  authDomain: firebaseConfig.FB_API_KEY2,
+  databaseURL: firebaseConfig.FB_API_KEY3,
+  projectId: firebaseConfig.FB_API_KEY4,
+  storageBucket: firebaseConfig.FB_API_KEY5,
+  messagingSenderId: firebaseConfig.FB_API_KEY6,
+  appId: firebaseConfig.FB_API_KEY7,
+  measurementId: firebaseConfig.FB_API_KEY8
 };
-
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-
-// Initialize Realtime Database and get a reference to the service
 const database = getDatabase(app);
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
-
-  // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  
