d886e976be32f44ec2a43ee47b57f6f09e752c19
사과포럼 <118648952+sagwaforum@users.noreply.github.com>
2025-05-31 21:57:15 +0900

    Update posts.js

--- posts.js
+++ posts.js
@@ -1,16 +1,16 @@
 const posts = [
   {
     id: 1,
-    title: "아이폰 16 루머 총정리",
-    summary: "2025년형 아이폰에 들어갈 주요 기능은 무엇일까?",
-    category: "iPhone",
+    title: "iOS 19, iOS 26으로 리브랜딩 된다.",
+    summary: "여러 외신에서 보도한 소식! iOS 26에서 만나게 될 기능들은?",
+    category: "iOS",
     date: "2025.05.31",
   },
   {
     id: 2,
-    title: "M4 칩 맥북, 드디어 출시",
-    summary: "애플 실리콘 3세대 M4 칩 장착 맥북 시리즈",
-    category: "Mac",
+    title: "오는 6월 10일 열리는 WWDC, OS 싹바꾼다",
+    summary: "애플은 이번 WWDC 개발자 회의에서 iOS 19와 더불어 새로운 OS들을 발표...",
+    category: "WWDC",
     date: "2025.05.30",
   },
 ];
