e40e9cadcf8f58abf8925e6eee621a4eb11cca73
사과포럼 <118648952+sagwaforum@users.noreply.github.com>
2025-05-31 22:08:55 +0900

    Update posts.js

--- posts.js
+++ posts.js
@@ -1,16 +1,18 @@
 const posts = [
   {
-    id: 1,
+    id: 2,
     title: "iOS 19, iOS 26으로 리브랜딩 된다.",
     summary: "여러 외신에서 보도한 소식! iOS 26에서 만나게 될 기능들은?",
     category: "iOS",
     date: "2025.05.31",
+    link: "2.html",
   },
   {
-    id: 2,
+    id: 1,
     title: "오는 6월 10일 열리는 WWDC, OS 싹바꾼다",
     summary: "애플은 이번 WWDC 개발자 회의에서 iOS 19와 더불어 새로운 OS들을 발표...",
     category: "WWDC",
     date: "2025.05.30",
+    link: "1.html",
   },
 ];
